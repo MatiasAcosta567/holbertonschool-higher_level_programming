6be75352132ccd70bda76b37f0680d0856142b95
Matias Acosta <2277@holbertonschool.com>
2020-12-10 17:02:07 +0000

    Fixing bugs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        num1 = argv[1]
+        n1 = int(argv[1])
         op = argv[2]
-        num2 = argv[3]
+        n2 = int(argv[3])
         if op != '+' and op != '-' and op != '*' and op != '/':
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
-        if op == '+':
-            print("{:d} {} {:d} = {:d}".format(num1, op, num2, add(num1, num2)))
-        if op == '-':
-            print("{:d} {} {:d} = {:d}".format(num1, op, num2, sub(num1, num2)))
-        if op == '*':
-            print("{:d} {} {:d} = {:d}".format(num1, op, num2, mov(num1, num2)))
-        if op == '/':
-            print("{:d} {} {:d} = {:d}".format(num1, op, num2, div(num1, num2)))
+        elif op == '+':
+            print("{:d} {} {:d} = {:d}".format(n1, op, n2, add(n1, n2)))
+        elif op == '-':
+            print("{:d} {} {:d} = {:d}".format(n1, op, n2, sub(n1, n2)))
+        elif op == '*':
+            print("{:d} {} {:d} = {:d}".format(n1, op, n2, mul(n1, n2)))
+        elif op == '/':
+            print("{:d} {} {:d} = {:d}".format(n1, op, n2, div(n1, n2)))
