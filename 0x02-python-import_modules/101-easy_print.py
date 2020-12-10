0e836fca91868b569e3713f5a75c624d3654f747
Matias Acosta <2277@holbertonschool.com>
2020-12-10 16:20:03 +0000

    fixing

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-import os
-os.system('echo "#pythoniscool"')
-#exec('imp%crt sys\nsys.stdout.wr%cte(%c%cpythoniscool%c%c),'%(111,105,34,35,121,34))
+import os;os.system('echo "#pythoniscool"')
