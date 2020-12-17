b2dcd894aefc005f9263f129903698ae4a0f6633
Matias Acosta <2277@holbertonschool.com>
2020-12-17 16:11:19 +0000

    Add privileges

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for par in sorted(a_dictionary):
-        print(par, a_dictionary.values())
+    my_list = list(sorted(a_dictionary))
+    for i in my_list:
+        print("{}: {}".format(i, a_dictionary.get(i)))
