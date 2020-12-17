cd4a606c890ed0708570daecfeb600aa98f382f7
Matias Acosta <2277@holbertonschool.com>
2020-12-17 20:04:01 +0000

    Fix 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,10 @@
 def best_score(a_dictionary):
     maximo = 0
     llave = ""
-    if a_dictionary is not None:
-        for key, value in a_dictionary.items():
-            if value > maximo:
-                llave = key
-                maximo = value
-        return llave
-    else:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
+    for key, value in a_dictionary.items():
+        if value > maximo:
+            llave = key
+            maximo = value
+    return llave
