1ce4a2bd736a42978c67309b50e480f2b74478e9
Matias Acosta <2277@holbertonschool.com>
2021-01-05 15:56:57 -0300

    Fix documentation and input valid

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Define a Square Class"""
+
+
 class Square:
+    """Square class"""
+
     def __init__(self, size=0):
-        try:
-            int(size)
-            if (size < 0):
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
+        """Constructor method"""
+        if type(size) != int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
+        """Method to calculate the area of square"""
         return self.__size * self.__size
