008939ca248c6a8161f9b83c74af727cf44951d5
Matias Acosta <2277@holbertonschool.com>
2021-01-05 20:22:58 -0300

    Fix

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@ import math
 
 class MagicClass:
     """Magic Class"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Constructor method"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
