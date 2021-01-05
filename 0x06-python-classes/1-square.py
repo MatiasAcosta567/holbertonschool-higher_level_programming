12d4659aac5af4773afa44e9b24e5bf9de815177
Matias Acosta <2277@holbertonschool.com>
2021-01-05 15:50:03 -0300

    Fix documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
+
 class Square:
+    """square class"""
+
     def __init__(self, size=0):
+        """Constructor method"""
         self.__size = size
