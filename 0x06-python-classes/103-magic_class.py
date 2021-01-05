30a41b1291e523ea84fb55044a068c92e602146c
Matias Acosta <2277@holbertonschool.com>
2021-01-05 19:09:16 -0300

    Advanced task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Magic module"""
+
+class MagicClass:
+    """Magic Class"""
+    def __init__(self, radius):
+        """Constructor method"""
+        if type(radius) != int and type(radius) != float:
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """return the area of a circle"""
+        return (self.radius ** 2) * math.pi
+
+    def circumference:
+        """retutrn a circumference of a circle"""
+        return (2 * math.pi) * self.radius
