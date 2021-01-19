0ec9f714c87fa784bfd0f328e9aa77915d3c7221
Matias Acosta <2277@holbertonschool.com>
2021-01-19 18:13:32 -0300

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module documentation"""
+
+
+def inherits_from(obj, a_class):
+    if issubclass(type(obj), a_class):
+        if type(obj) == a_class:
+            return False
+        else:
+            return True
+    else:
+        return False
