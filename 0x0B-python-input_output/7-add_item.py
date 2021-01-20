1cf153c70fd76335f3250a856a5ea2bbac232b23
Matias Acosta <2277@holbertonschool.com>
2021-01-20 20:15:42 +0000

    Fix documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,10 @@
 """imports"""
 import json
 from sys import argv
-"""Module documentation"""
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Module documentation"""
+
 
 file_name = "add_item.json"
 try:
