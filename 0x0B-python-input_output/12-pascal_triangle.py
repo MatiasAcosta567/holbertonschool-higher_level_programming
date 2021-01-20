efd432b2fd8a82ebe1d632b4ab1222600b19a093
Matias Acosta <2277@holbertonschool.com>
2021-01-20 22:10:18 +0000

    Ex 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,5 +13,5 @@ def pascal_triangle(n):
         row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] +
             prev_row[j] or 1 for j in range(0, i)]
         prev_row = row
-    triangle += [row]
+        triangle += [row]
     return triangle[1:]
