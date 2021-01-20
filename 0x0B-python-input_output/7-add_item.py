1523f9c542e53b0f8887442629234a68ae6b4f5a
Matias Acosta <2277@holbertonschool.com>
2021-01-20 20:16:09 +0000

    Fix documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """imports"""
-import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
