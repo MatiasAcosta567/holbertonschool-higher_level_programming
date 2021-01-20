7214bea4f2c94b28cf62e25a9200e23bc577430b
Matias Acosta <2277@holbertonschool.com>
2021-01-20 20:10:33 +0000

    Fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,11 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-elems = []
 file_name = "add_item.json"
-if os.path.exists(file_name):
-    file_list = load_from_json_file(file_name)
-    for elements in file_list:
-        elems.append(elements)
+try:
+    elems = load_from_json_file(file_name)
+except:
+    elems = []
 for i in range(1, len(argv)):
     elems.append(argv[i])
 save_to_json_file(elems, file_name)
