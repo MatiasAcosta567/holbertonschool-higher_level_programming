68804f1cd825ce0b2ae1f26cbac18fdf006bdd5f
Matias Acosta <2277@holbertonschool.com>
2021-01-20 20:26:49 +0000

    Fix

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""import"""
+"""Function that writes an Object to text file, using a JSON representation """
+
+
 import json
-"""Module documentation"""
 
 
 def save_to_json_file(my_obj, filename):
     """function that convert json obj to file"""
+
+
     with open(filename, mode="w", encoding="utf-8") as f:
             f.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""import"""
-import json
-"""Module documentation"""
+"""Function that creates an Object from a JSON file"""
 
+
+import json
 
 def load_from_json_file(filename):
     """function that create an object from a json file"""
+
+
     with open(filename, encoding="utf-8") as f:
         return json.load(f)
