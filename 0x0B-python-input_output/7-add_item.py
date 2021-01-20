41d196d36a1cf21203de4181b1ebf99d1f1a4a15
Matias Acosta <2277@holbertonschool.com>
2021-01-20 20:17:52 +0000

    Fix documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """Module documentation"""
 
-
-file_name = "add_item.json"
 try:
-    elems = load_from_json_file(file_name)
+    elems = load_from_json_file("add_item.json")
 except:
     elems = []
 for i in argv[1:]:
