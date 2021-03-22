be7b77e739b42c683c53ae8ccd8f4218469488be
Matias Acosta <2277@holbertonschool.com>
2021-03-22 13:19:01 -0300

    fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,7 @@
-#!/usr/bin/node
-let idx = 0;
-
-function biggest (arr) {
-  let biggest = 0;
-  let indx = 0;
-  arr.forEach((val, index) => {
-    if (val > biggest) {
-      biggest = val;
-      indx = index;
-    }
-  });
-  return (indx);
-}
-let copy = process.argv.slice();
-if (copy.length <= 3) {
+#!/usr/local/bin/node
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  idx = biggest(copy);
-  copy.splice(idx, 1);
-  idx = biggest(copy);
-  console.log(copy[idx]);
+  process.argv.sort();
+  console.log(process.argv[process.argv.length - 2]);
 }
