e9288a2a634864435c4869b5823c316a220bba77
Matias Acosta <2277@holbertonschool.com>
2021-03-22 12:43:18 -0300

    New tasks

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+function factorial(a)
+{
+    if (isNaN(a)){
+        return (1);
+    }
+    if (a === 0){
+        return (1);
+    }
+    return a * factorial(a - 1);
+}
+
+console.log(factorial(process.argv[2]));
