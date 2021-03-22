a83840afd0cf1e7a51bad84b71c7fb1133fd4e03
Matias Acosta <2277@holbertonschool.com>
2021-03-22 13:31:21 -0300

    Fix Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  process.argv.sort();
-  console.log(process.argv[process.argv.length - 2]);
+  const copy = process.argv.slice(2, process.argv.length);
+  copy.map(Number).sort((a, b) => a - b);
+  console.log(copy[copy.length - 2]);
 }
