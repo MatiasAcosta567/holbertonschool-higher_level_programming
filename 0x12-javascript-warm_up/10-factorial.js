291f272fee9bb0fc66715266257d6823680900e0
Matias Acosta <2277@holbertonschool.com>
2021-03-22 12:45:19 -0300

    Fix semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-function factorial(a)
-{
-    if (isNaN(a)){
-        return (1);
-    }
-    if (a === 0){
-        return (1);
-    }
-    return a * factorial(a - 1);
+function factorial (a) {
+  if (isNaN(a)) {
+    return (1);
+  }
+  if (a === 0) {
+    return (1);
+  }
+  return a * factorial(a - 1);
 }
 
 console.log(factorial(process.argv[2]));
