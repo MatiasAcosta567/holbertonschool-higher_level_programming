1500bc8d38fd2977aa4c776668fc3ccab0a1b4ed
Matias Acosta <2277@holbertonschool.com>
2021-03-22 14:01:27 -0300

    Advanced task

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = () => {
+  myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
