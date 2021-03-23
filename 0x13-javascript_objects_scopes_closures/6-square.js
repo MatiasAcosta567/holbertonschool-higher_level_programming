91e9c95097e934f25a1793c04b2a5fc3e1161289
Matias Acosta <2277@holbertonschool.com>
2021-03-23 12:36:51 -0300

    Initial commit with mandatory tasks

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let square = '';
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        square += c != null ? c : 'X';
+      }
+      if (y < this.height - 1) {
+        square += '\n';
+      }
+    }
+    console.log(square);
+  }
+}
+
+module.exports = Square;
