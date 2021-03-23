955cf87d940da163b844c6637440b1a1da47ff2b
Matias Acosta <2277@holbertonschool.com>
2021-03-23 13:44:01 -0300

    Advanced Task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/local/bin/node
+const dict = require('./101-data').dict;
+const keys = Object.keys(dict);
+const newDict = {};
+for (const elem in keys) {
+  console.log(dict[keys[elem]]);
+  console.log(keys[elem]);
+  if (newDict.hasOwnProperty(dict[keys[elem]])) {
+    newDict[dict[keys[elem]]].push(keys[elem]);
+  } else {
+    newDict[dict[keys[elem]]] = [keys[elem]];
+  }
+}
+console.log(newDict);
