bd4ea00f4a295b430c1ef5b27ef9c1730fee2099
Matias Acosta <2277@holbertonschool.com>
2021-03-23 14:36:22 -0300

    Advanced task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,14 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const fs = require('fs');
+
+let data = fs.readFileSync(fileA,
+  { encoding: 'utf8', flag: 'r' });
+
+data += fs.readFileSync(fileB,
+  { encoding: 'utf8', flag: 'r' });
+
+fs.writeFileSync(fileC, data);
