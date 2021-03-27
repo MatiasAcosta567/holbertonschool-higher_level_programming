0f074e5a0b3e4ff3922447a3177e520a122995c3
Matias Acosta <2277@holbertonschool.com>
2021-03-27 12:19:43 -0300

    Fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
         Session = sessionmaker(bind=engine)
         session = Session()
         for state in session.query(State).order_by(State.id):
-            if ('a' in state.name):
+            if "a" in state.name:
                 print("{}: {}".format(state.id, state.name))
         session.close() 
