6319033ffb5fe2d2c5a3348a279e78833a07437b
Matias Acosta <2277@holbertonschool.com>
2021-04-08 16:12:18 -0300

    Advanced task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 
 def find_peak(li):
+    """Find a peak in a unsorted list of integers"""
     peak = []
     for index in range(1, len(li) - 1):
         if li[index] >= li[index - 1] and li[index] >= li[index + 1]:
