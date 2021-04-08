0c8b785c7282aec00f4ccdf5c86b61e04151658e
Matias Acosta <2277@holbertonschool.com>
2021-04-08 16:22:13 -0300

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,10 +5,18 @@
 def find_peak(li):
     """Find a peak in a unsorted list of integers"""
     peak = []
-    for index in range(1, len(li) - 1):
-        if li[index] >= li[index - 1] and li[index] >= li[index + 1]:
-            peak.append(li[index])
-    if len(peak) == 0:
+    size = len(li)
+    if li is None:
         return None
+    if size == 1:
+        return li[0]
+    elif size == 2:
+        return max(li)
     else:
-        return max(peak, key=int)
+        for index in range(1, size - 1):
+            if li[index] >= li[index - 1] and li[index] >= li[index + 1]:
+                peak.append(li[index])
+        if len(peak) == 0:
+            return None
+        else:
+            return max(peak, key=int)
