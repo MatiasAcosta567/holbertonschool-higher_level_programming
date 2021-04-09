c8adcdffaad3730f24e6bf84f7c419d7ce08b48f
Matias Acosta <2277@holbertonschool.com>
2021-04-09 16:21:11 -0300

    Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter,
+and finally displays the body of the response"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    data = {"email": argv[2]}
+
+    page = requests.post(url, data=data)
+    print(page.text)
