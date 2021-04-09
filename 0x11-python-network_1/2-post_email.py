c686813a8b4d66310dfc40cb4b82fccb36fe3020
Matias Acosta <2277@holbertonschool.com>
2021-04-09 15:36:37 -0300

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found
+in the header of the response"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+url = sys.argv[1]
+values = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    page = response.read()
+
+print(page.decode())
