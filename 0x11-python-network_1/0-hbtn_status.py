63edc9765c3f961229c9d8bdf801ca81229e0da9
Matias Acosta <2277@holbertonschool.com>
2021-04-09 12:58:14 -0300

    Fix pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,6 @@ import urllib.request
 '''Python script that fetches https://intranet.hbtn.io/status'''
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
+    html = response.read()
 print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-      .format(type(html),html, html.decode()))
+      .format(type(html), html, html.decode()))
