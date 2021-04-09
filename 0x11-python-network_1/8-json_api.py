52448903f1420da2d5ffa9e928cbecddd38b5128
Matias Acosta <2277@holbertonschool.com>
2021-04-09 17:09:16 -0300

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = {"q": argv[1] if len(argv) == 2 else ""}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=data)
+    json_resp = response.json()
+    if len(json_resp) == 0:
+        print("No result")
+    else:
+        print("[{}] {}".format(json_resp.get("id"), json_resp.get("name")))
