9d07bdd7d3be77f75c06c3c3d4977cf6778b27be
Matias Acosta <2277@holbertonschool.com>
2021-04-09 16:02:59 -0300

    Fix pep8

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,5 @@ import requests
 
 if __name__ == '__main__':
     page = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(page.text), page.text))
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(page.text), page.text))
