ecff9dd65eb26838f8d852a9886d3029f38e6263
Matias Acosta <2277@holbertonschool.com>
2021-04-09 18:18:25 -0300

    Advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that takes 2 arguments in order to solve this challenge."""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/" + owner + "/" + repo + "/commits"
+    response = requests.get(url)
+    commits = response.json()
+    for index, elem in enumerate(commits):
+        if index <= 9:
+            author = elem.get('commit').get('author').get('name')
+            code = elem.get('commit').get('tree').get('sha')
+            print("{}: {}".format(code, author))
+        else:
+            break
