598c9dc6a72b8351b6154575f3e47f88e20ab6ba
Matias Acosta <2277@holbertonschool.com>
2021-04-09 15:41:15 -0300

    Fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ = "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
+    data = data.encode('utf8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
