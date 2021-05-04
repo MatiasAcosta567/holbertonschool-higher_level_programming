3199a5312b181782224fc211e04beb4d9c8b5305
Matias Acosta <2277@holbertonschool.com>
2021-05-04 13:58:06 -0300

    Advanced Task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,12 +5,17 @@ const api = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(api, function (err, res, body) {
   if (!err) {
     const characters = JSON.parse(body).characters;
-    characters.forEach(element => {
-      request(element, function (err, res, body) {
-        if (!err) {
-          return console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    printChar(characters, 0);
   }
 });
+
+function printChar (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printChar(characters, index + 1);
+      }
+    }
+  });
+}
